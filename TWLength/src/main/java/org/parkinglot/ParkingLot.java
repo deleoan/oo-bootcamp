e0fba637aef878e36b66e14c214fb7f79dbc0c35
deleoan <ana.katrina.de.leon@oocl.com>
2018-06-12 23:16:18 +0800

    multiple parking handling

--- TWLength/src/main/java/org/parkinglot/ParkingLot.java
+++ TWLength/src/main/java/org/parkinglot/ParkingLot.java
@@ -4,38 +4,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 class ParkingLot {
-    private int availableSlot;
-    private List<Car> cars = new ArrayList<>();
-
-    ParkingLot(int availableSlot) {
-        this.availableSlot = availableSlot;
-    }
+    private List<Car> parkingLotA = new ArrayList<>();
+    private List<Car> parkingLotB = new ArrayList<>();
+    private List<Car> parkingLotC = new ArrayList<>();
 
     Ticket checkSlotAndParkCar(Car car) {
-        if(checkSlotAvailability() && car != null){
-            parkCar(car);
-            return car.getCarTicket();
+        if (car != null) {
+            if (hasAvailableSlot(parkingLotA)) {
+                parkCar(car, parkingLotA);
+                return car.getCarTicket();
+            } else if (hasAvailableSlot(parkingLotB)) {
+                parkCar(car, parkingLotB);
+                return car.getCarTicket();
+            } else if (hasAvailableSlot(parkingLotC)){
+                parkCar(car, parkingLotC);
+                return car.getCarTicket();
+            } else {
+                return null;
+            }
         }
         return null;
     }
 
-    private boolean checkSlotAvailability() {
-        return this.availableSlot > 0;
+    private boolean hasAvailableSlot(List<Car> parkingLot) {
+        int parkingAvailableSlot = 1;
+        return parkingLot.size() < parkingAvailableSlot;
     }
 
-    private void parkCar(Car car) {
-        this.availableSlot -= 1;
-        cars.add(car);
+    private void parkCar(Car car, List<Car> parkingLot) {
+        parkingLot.add(car);
         int ticketNumber = 1001;
         Ticket ticket = new Ticket(ticketNumber);
         car.setCarTicket(ticket.getTicket());
     }
 
-    Car releaseCar(Ticket ticket) {
-        for(Car car : this.cars){
+    Car releaseCar(Ticket ticket, String parkingLot) {
+        List<Car> parking = getParkingLot(parkingLot);
+        assert parking != null;
+        for(Car car : parking){
             if(car.getCarTicket().ticketNumber == ticket.ticketNumber){
-                this.availableSlot += 1;
+                parking.remove(0);
                 return car;
+            }
+        }
+        return null;
+    }
+
+    private List<Car> getParkingLot(String parkingLot) {
+        if (!parkingLot.isEmpty()) {
+            switch (parkingLot) {
+                case "parkingLotA":
+                    return new ArrayList<>(parkingLotA);
+                case "parkingLotB":
+                    return new ArrayList<>(parkingLotB);
+                case "parkingLotC":
+                    return new ArrayList<>(parkingLotC);
             }
         }
         return null;
--- TWLength/src/test/java/org/parkinglot/ParkingLotTest.java
+++ TWLength/src/test/java/org/parkinglot/ParkingLotTest.java
@@ -8,7 +8,7 @@ public class ParkingLotTest {
     @Test
     public void park_car_to_available_slot() {
         Car car = new Car();
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot();
 
         Assert.assertNotNull(parkingLot.checkSlotAndParkCar(car));
     }
@@ -16,7 +16,10 @@ public class ParkingLotTest {
     @Test
     public void parking_lot_is_full(){
         Car car = new Car();
-        ParkingLot parkingLot = new ParkingLot(0);
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.checkSlotAndParkCar(car);
+        parkingLot.checkSlotAndParkCar(car);
+        parkingLot.checkSlotAndParkCar(car);
 
         Assert.assertNull(parkingLot.checkSlotAndParkCar(car));
     }
@@ -24,71 +27,55 @@ public class ParkingLotTest {
     @Test
     public void return_parked_car(){
         Car car = new Car();
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot();
         parkingLot.checkSlotAndParkCar(car);
 
-        Assert.assertNotNull(parkingLot.releaseCar(new Ticket(1001)));
+        Assert.assertNotNull(parkingLot.releaseCar(new Ticket(1001), "parkingLotA"));
     }
 
     @Test
     public void should_not_return_parked_car_no_ticket(){
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot();
 
-        Assert.assertNull(parkingLot.releaseCar(null));
+        Assert.assertNull(parkingLot.releaseCar(null, "parkingLotA"));
     }
 
     @Test
     public void no_car_but_with_ticket(){
         Ticket ticket = new Ticket(0001);
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = new ParkingLot();
 
-        Assert.assertNull(parkingLot.releaseCar(ticket));
+        Assert.assertNull(parkingLot.releaseCar(ticket, "parkingLotA"));
     }
 
     @Test
     public void lot_A_is_full_should_try_to_park_in_lot_B() {
         Car car = new Car();
-        ParkingLot parkingLotA = new ParkingLot(0);
-        Ticket parkingTicket = parkingLotA.checkSlotAndParkCar(car);
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.checkSlotAndParkCar(car);
 
-        if (parkingTicket == null) {
-            ParkingLot parkingLotB = new ParkingLot(1);
-            parkingTicket = parkingLotB.checkSlotAndParkCar(car);
-
-            Assert.assertNotNull(parkingTicket);
-        }
+        Assert.assertNotNull(parkingLot.checkSlotAndParkCar(car));
     }
 
     @Test
     public void lot_A_and_B_is_full_should_try_to_park_in_lot_C() {
         Car car = new Car();
-        ParkingLot parkingLotA = new ParkingLot(0);
-        Ticket parkingTicket = parkingLotA.checkSlotAndParkCar(car);
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.checkSlotAndParkCar(car);
+        parkingLot.checkSlotAndParkCar(car);
 
-        if (parkingTicket == null) {
-            ParkingLot parkingLotB = new ParkingLot(0);
-            parkingTicket = parkingLotB.checkSlotAndParkCar(car);
-            if (parkingTicket == null) {
-                ParkingLot parkingLotC = new ParkingLot(1);
-                parkingTicket = parkingLotC.checkSlotAndParkCar(car);
-
-                Assert.assertNotNull(parkingTicket);
-            }
-        }
+        Assert.assertNotNull(parkingLot.checkSlotAndParkCar(car));
     }
 
     @Test
-    public void cannot_see_car_in_lot_A_pickup_in_lot_B() {
+    public void car_not_in_lot_A_pickup_in_lot_B() {
         Ticket parkingTicket = new Ticket(1001);
-        ParkingLot parkingLotA = new ParkingLot(0);
-        ParkingLot parkingLotB = new ParkingLot(1);
-        parkingLotB.checkSlotAndParkCar(new Car());
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.checkSlotAndParkCar(car);
+        parkingLot.checkSlotAndParkCar(car);
 
-        Car releasedCar = parkingLotA.releaseCar(parkingTicket);
-        if(releasedCar == null) {
-            releasedCar = parkingLotB.releaseCar(parkingTicket);
-
-            Assert.assertNotNull(releasedCar);
-        }
+        Assert.assertNotNull(parkingLot.releaseCar(parkingTicket, "parkingLotB"));
     }
+
 }
