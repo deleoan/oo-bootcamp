1eea4fba3978961e67daeb488de2b13f5430297b
deleoan <ana.katrina.de.leon@oocl.com>
2018-06-17 22:17:34 +0800

    added class for super parking boy

---
+++ TWLength/src/main/java/org/parkinglot/MostAvailableSpaceRateParkingLotChecker.java
@@ -0,0 +1,10 @@
+package org.parkinglot;
+
+import java.util.List;
+
+public class MostAvailableSpaceRateParkingLotChecker implements ParkingLotChecker {
+    @Override
+    public ParkingLot getAvailableParkingLot(List<ParkingLot> parkingLots) {
+        return null;
+    }
+}
